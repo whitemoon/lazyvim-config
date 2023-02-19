98ae7bce23ab0743a9c20187ba1143a3534177c2
whitemoon <woshixujiaojie@163.com>
2023-02-19 20:03:30 +0800

    enable code fold base on treesitter

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,4 +2,10 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- parser gbk file
 vim.opt.fileencodings = "utf-8,gbk"
+
+-- code fold
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+vim.opt.foldlevel = 99
