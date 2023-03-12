4e55ca63a80261495820b4a327166940eee5113d
whitemoon <woshixujiaojie@163.com>
2023-03-12 15:56:20 +0800

    del: git blame and diffview

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,25 +1,8 @@
 return {
-  -- git blame
-  {
-    "f-person/git-blame.nvim",
-    event = "BufReadPre",
-  },
-
   -- git conflict
   {
     "akinsho/git-conflict.nvim",
     event = "BufReadPre",
     config = true,
-  },
-
-  -- git better diffing
-  {
-    "sindrets/diffview.nvim",
-    cmd = { "DiffviewOpen", "DiffviewClose", "DiffviewToggleFiles", "DiffviewFocusFiles" },
-    config = true,
-    keys = {
-      { "<leader>gd", "<cmd>DiffviewOpen<cr>", desc = "DiffView Open" },
-      { "<leader>gdc", "<cmd>DiffviewClose<cr>", desc = "DiffView Close" },
-    },
   },
 }
